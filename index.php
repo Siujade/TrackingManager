e7cd72f602bf1ac4d625831897566ed59b9e757f
siujade <siujade@gmail.com>
2016-12-26 15:11:54 +0200

    Adding jquery script

--- index.php
+++ index.php
@@ -1,14 +1,17 @@
 <?php
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Tracking Manager</title>
+    <script src="resources/javascript/jquery-3.1.1.slim.min.js"></script>
 </head>
 <body>
 <p>HELLO!</p>
+<script>
+    $('p').text('HELLO THEREE...');
+</script>
 </body>
 </html>
